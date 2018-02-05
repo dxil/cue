b6ce5cdb19b633992927cb67e472cd3a84e094d6
cuzz <cuzz@xqopen.cn>
2018-02-05 19:00:18 +0800

    增加对extend的判断

--- test/units/specs/src/utils_spec.js
+++ test/units/specs/src/utils_spec.js
@@ -1,7 +1,19 @@
+const utils = require('../../../../src/utils')
+const assert = require('assert');
+
 describe('utils', function () {
   describe('extend', function () {
     it('extend 可以拓展对象', function () {
-
+      let a = {}
+      let b = {a:1}
+      let c = {b:2}
+      assert.deepEqual(utils.extend(a, b, c), {a:1,b:2})
+    })
+    it('extend 循环引用不拓展', function () {
+      let a = {}
+      let b = {a:1}
+      let c = {b:a}
+      assert.deepEqual(utils.extend(a, b, c), {a:1})
     })
   })
 })
